b374c5b787d737603ce79ffc56c5e2287515cc9b
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2025-03-16 00:10:15 -0300

    feat: :sparkles: add route file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AddressFormComponent } from './address-form/address-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AddressFormComponent,
+  },
+];
